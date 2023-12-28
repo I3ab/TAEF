25bfe01754f21766ccae17831c257b96912af4c7
ImageIntelLab <ImageIntelLab@outlook.com>
2023-12-28 14:06:45 +0800

    Update D3_to_2D.m

--- data/D3_to_2D.m
+++ data/D3_to_2D.m
@@ -1,8 +1,4 @@
 function [Y]=D3_to_2D(data,col)
-%% 2021.1.13 by addeng
-%3D HSI to 2D
-%col, col=1
-%row, col=0(default)
 
 if (nargin<2)
     col=0;
@@ -10,7 +6,7 @@ end
 
 [H,W,B]=size(data);
 if col==1    
-    Y=reshape(data,[H*W,B])';%N按列排
+    Y=reshape(data,[H*W,B])';
 end
 
 data_trans=zeros(W,H,B);
@@ -18,7 +14,7 @@ if col==0
     for i=1:B
         data_trans(:,:,i)=data(:,:,i)';
     end
-    Y=reshape(data_trans,[H*W,B])';%N按行排
+    Y=reshape(data_trans,[H*W,B])';
 end
 
 end
